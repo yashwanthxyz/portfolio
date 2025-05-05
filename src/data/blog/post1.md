5391ca6782bc6532bdeba6709f665716e1541d9c
yashwanthxyz <yashwanthkosuri29@gmail.com>
2025-05-05 14:48:55 +0530

    new version

---
+++ src/data/blog/post1.md
@@ -0,0 +1,8 @@
+---
+title: "LINUX is fun."
+description: "this is a post example"
+pubDate: 2025-05-05
+createdAt: 2025-05-05T13:45:00.000Z
+category: "intro"
+draft: false
+---
---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const LINKS = {
+  github: "https://github.com/yashwanthxyz",
+  linkedin: "https://www.linkedin.com/in/yashwanthxyz/",
+  medium: "https://medium.com/@yashwanthxyz",
+  discord: "https://discordapp.com/users/163300027618295808",
+  dribble: "https://dribbble.com/Ladvace_Jace",
+  email: "yashwanthkosuri29@gmail.com",
+  resume: "https://yashwanthxyz-resume.vercel.app/",
+  x: "https://x.com/yashwanthxyz",
+};
+
+export const loaderAnimation = [
+  ".loader",
+  { opacity: [1, 0], pointerEvents: "none" },
+  { easing: "ease-out" },
+];
