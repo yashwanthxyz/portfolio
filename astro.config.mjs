cfc1728a1cbeba77340cb2c5cfee153ae88b38e8
yashwanthxyz <yashwanthkosuri29@gmail.com>
2025-05-05 14:59:59 +0530

    added: vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,22 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://your-domain.com", // Update this with your actual domain once deployed
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
+        "https://your-domain.com/sitemap-index.xml",
+        "https://your-domain.com/sitemap-0.xml",
       ],
     }),
     solidJs(),
@@ -30,7 +28,7 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime],
   },
   output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
+  adapter: vercel(),
   vite: {
     assetsInclude: "**/*.riv",
   },
