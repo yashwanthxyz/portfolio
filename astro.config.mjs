6519c98f3c34a5ee8b0d40ff5b4f492eb225b3cb
yashwanthxyz <yashwanthkosuri29@gmail.com>
2025-05-06 09:30:21 +0530

    new version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,9 @@
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
-import icon from "astro-icon";
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://yashwanthxyz.vercel.app",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://yashwanthxyz.vercel.app/sitemap-index.xml",
-        "https://yashwanthxyz.vercel.app/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  output: "server",
-  adapter: vercel(),
-  vite: {
-    assetsInclude: "**/*.riv",
-  },
+  site: "https://astro-nano-demo.vercel.app",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/data/blog/post1.md
+++
@@ -1,8 +0,0 @@
----
-title: "LINUX is fun."
-description: "this is a post example"
-pubDate: 2025-05-05
-createdAt: 2025-05-05T13:45:00.000Z
-category: "intro"
-draft: false
----
